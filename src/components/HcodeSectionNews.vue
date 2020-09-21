190da7e07dbcabcaf6f1955866f8fc9853194417
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-21 00:18:55 -0300

    tornando dinamica a estrutura de noticias, usando filtro tuncate

--- src/components/HcodeSectionNews.vue
+++ src/components/HcodeSectionNews.vue
@@ -1,9 +1,28 @@
 <template>
     <section>
         <div class="container">
-            <HcodeSectionNewsIndividual/>
-            <HcodeSectionNewsIndividual/>
-            <HcodeSectionNewsIndividual/>
+            <HcodeSectionNewsIndividual
+            img-name="news1.jpg"
+            img-info="Notícia 1"
+            news-title="Começam treinos para a nova temporada"
+            news-content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?"
+            news-date="20/09/2020"
+            />
+
+            <HcodeSectionNewsIndividual
+            img-name="news2.jpg"
+            img-info="Notícia 2"
+            news-title="Jogo de quarta-feira termina empatado"
+            news-content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?"
+            news-date="21/09/2020"
+            />
+            <HcodeSectionNewsIndividual
+            img-name="news3.jpg"
+            img-info="Notícia 3"
+            news-title="A inauguração do novo estádio será na semana que vem"
+            news-content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?"
+            news-date="02/10/2020"
+            />
         </div>
     </section>
 </template>
--- src/components/HcodeSectionNewsIndividual.vue
+++ src/components/HcodeSectionNewsIndividual.vue
@@ -1,21 +1,42 @@
 <template>
     <div class="row">
         <div class="col-3">
-            <img src="../assets/news1.jpg" alt="Notícia 1">
+            <img :src="require('../assets/' + this.imgName)" :alt="imgInfo">
         </div>
         <div class="col-9">
-            <h2>Começam treinos para a nova temporada</h2>
+            <h2>{{newsTitle}}</h2>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?</p>
+            <p>{{newsContent | truncate(200)}}</p>
 
-            <span class="font-italic">20/09/2020</span>
+            <span class="font-italic">{{newsDate}}</span>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        imgName: {
+            type: String,
+            required: true
+        },
+        imgInfo: {
+            type: String,
+            required: true
+        },
+        newsTitle: {
+            type: String,
+            required: true
+        },
+        newsContent: {
+            type: String,
+            required: true
+        },
+        newsDate: {
+            type: String,
+            required: true
+        }
+    }
 }
 </script>
 
