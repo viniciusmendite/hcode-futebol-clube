435d6967f849735fdc0734ac9cbd31c8143ec240
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-21 00:51:30 -0300

    utilizando v-model para receber o nome do campeonato digitado

---
+++ src/components/HcodeInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <input
+    type="text"
+    :value="value"
+    @keyup="$emit('input', $event.target.value)"
+    class="form-control"
+    placeholder="Digite seu clube">
+</template>
+
+<script>
+export default {
+    props: {
+        value: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/HcodeSection.vue
+++ src/components/HcodeSection.vue
@@ -7,17 +7,36 @@
       </div>
     <HcodeSectionBanner />
     <HcodeSectionNews />
+
+    <div class="container">
+        <div class="row my-club mt-5">
+            <div class="col-6">
+                <h2>Seu clube é: {{myClub}}</h2>
+            </div>
+            <div class="col-6">
+                <HcodeInput v-model="myClub"  />
+            </div>
+        </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import HcodeSectionBanner from './HcodeSectionBanner';
 import HcodeSectionNews from './HcodeSectionNews';
+import HcodeInput from './HcodeInput'
 
 export default {
   components: {
     HcodeSectionBanner,
     HcodeSectionNews,
+    HcodeInput
+  },
+  data() {
+      return {
+          myClub: 'Hcode Treinamentos'
+      }
   },
   props: {
       championship: String
