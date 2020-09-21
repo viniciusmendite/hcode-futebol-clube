8dfe411a7871576e7e9c58c18cf8b01532f47b3d
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-21 08:13:04 -0300

    carregando noticias usando os getters

--- src/components/HcodeSectionNews.vue
+++ src/components/HcodeSectionNews.vue
@@ -23,43 +23,16 @@
 
 <script>
 import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual'
+import { mapGetters } from 'vuex';
 
 export default {
     components: {
         HcodeSectionNewsIndividual,
     },
-    data() {
-        return {
-            news: []
-        }
-    },
-    created() {
-        this.news = [
-        {
-            id: 1,
-            title: 'Começam treinos para a nova temporada',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?',
-            date: '2020-09-20',
-            img: 'news1.jpg',
-            imgInfo: 'Notícia 1'
-        },
-                {
-            id: 2,
-            title: 'Jogo de quarta-feira termina empatado',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?',
-            date: '2020-09-21',
-            img: 'news2.jpg',
-            imgInfo: 'Notícia 2'
-        },
-        {
-            id: 3,
-            title: 'A inauguração do novo estádio será na semana que vem',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?',
-            date: '2020-10-02',
-            img: 'news2.jpg',
-            imgInfo: 'Notícia 2'
-        }
-        ]
+    computed: {
+        ...mapGetters({
+            news: 'getNews'
+        })
     }
 }
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,33 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         championship: 'Campeonato Brasileiro',
-        clubName: 'Hcode Treinamentos'
+        clubName: 'Hcode Treinamentos',
+        news: [
+            {
+                id: 1,
+                title: 'Começam treinos para a nova temporada',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?',
+                date: '2020-09-20',
+                img: 'news1.jpg',
+                imgInfo: 'Notícia 1'
+            },
+                    {
+                id: 2,
+                title: 'Jogo de quarta-feira termina empatado',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?',
+                date: '2020-09-21',
+                img: 'news2.jpg',
+                imgInfo: 'Notícia 2'
+            },
+            {
+                id: 3,
+                title: 'A inauguração do novo estádio será na semana que vem',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias, ut voluptatum corporis explicabo, consequatur ipsum facere magnam et dicta tenetur maxime aspernatur delectus laborum deserunt beatae optio eum culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus saepe recusandae voluptatum dolores totam inventore praesentium nemo suscipit aliquid repellat cupiditate rerum culpa eveniet magnam, rem quos ducimus esse quasi?',
+                date: '2020-10-02',
+                img: 'news2.jpg',
+                imgInfo: 'Notícia 2'
+            }
+        ]
     },
     getters: {
         getChampionship(state) {
@@ -14,6 +40,9 @@ export default new Vuex.Store({
         },
         getClubName(state) {
             return state.clubName;
+        },
+        getNews(state) {
+            return state.news;
         }
     }
 })
